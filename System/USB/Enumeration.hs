64c8ca05aa7e57469661a9c0fdd365715bb29457
Bas van Dijk <v.dijk.bas@gmail.com>
2009-12-09 12:16:16 +0100

    added System/USB/Enumeration.hs
    Ignore-this: aa3921b05cc579eb2b6eb29b6515a8a7
    
    darcs-hash:20091209111616-4237f-95889d4f751c3d82eb8738df20b843737252decb.gz

---
+++ System/USB/Enumeration.hs
@@ -0,0 +1,22 @@
+--------------------------------------------------------------------------------
+-- |
+-- Module      :  System.USB.Devices
+-- Copyright   :  (c) 2009 Bas van Dijk
+-- License     :  BSD3 (see the file LICENSE)
+-- Maintainer  :  Bas van Dijk <v.dijk.bas@gmail.com>
+--
+-- This module provides functionality for enumerating the USB devices currently
+-- attached to the system.
+--
+--------------------------------------------------------------------------------
+
+module System.USB.Enumeration
+    ( Device
+    , getDevices
+
+    , busNumber
+    , deviceAddress
+    , deviceDesc
+    ) where
+
+import System.USB.Internal
