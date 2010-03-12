3f6bf74f2601315949e3fa6cb870e2d07a14b1a6
Bas van Dijk <v.dijk.bas@gmail.com>
2010-03-12 15:52:06 +0100

    Unicodified Enumerator and more explicit imports
    Ignore-this: c8a3aef5b0b9c4518465e8fcd0ca74f5
    
    darcs-hash:20100312145206-ae560-f75f415c66fedf587a41c1b0d21007e350ce0555.gz

--- System/USB/IO/Synchronous/Enumerator.hs
+++ System/USB/IO/Synchronous/Enumerator.hs
@@ -1,5 +1,4 @@
-{-# LANGUAGE NoImplicitPrelude #-}
-{-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE UnicodeSyntax, NoImplicitPrelude, ScopedTypeVariables #-}
 
 --------------------------------------------------------------------------------
 -- |
@@ -8,7 +7,7 @@
 -- License     :  BSD3 (see the file LICENSE)
 -- Maintainer  :  Bas van Dijk <v.dijk.bas@gmail.com>
 --
--- /Experimental/ enumerators for endpoints.
+-- /Untested/ /experimental/ enumerators for endpoints.
 --
 --------------------------------------------------------------------------------
 
@@ -19,15 +18,12 @@ module System.USB.IO.Synchronous.Enumerator
 
 
 --------------------------------------------------------------------------------
-
--- from myself:
-import System.USB.Internal
+-- Imports
+--------------------------------------------------------------------------------
 
 -- from base:
-import Prelude               ( (*), fromIntegral, undefined )
-import Data.Function         ( ($), (.) )
-import Data.Eq               ( (/=) )
-import Data.Bool             ( (&&) )
+import Prelude               ( fromIntegral, undefined )
+import Data.Function         ( ($) )
 import Data.Int              ( Int )
 import Data.Maybe            ( Maybe(Nothing, Just) )
 import Control.Monad         ( Monad, return, (>>=), fail )
@@ -37,113 +33,133 @@ import Foreign.Marshal.Alloc ( malloc, mallocBytes, free )
 import Foreign.Storable      ( Storable, peek, sizeOf )
 import Foreign.Ptr           ( Ptr, castPtr )
 
+-- from base-unicode-symbols:
+import Prelude.Unicode       ( (⋅) )
+import Data.Function.Unicode ( (∘) )
+import Data.Eq.Unicode       ( (≢) )
+import Data.Bool.Unicode     ( (∧) )
+
 -- from bindings-libusb:
-import Bindings.Libusb
+import Bindings.Libusb ( c'libusb_bulk_transfer, c'libusb_interrupt_transfer
+                       , c'LIBUSB_SUCCESS, c'LIBUSB_ERROR_TIMEOUT
+                       )
 
 -- from transformers:
-import Control.Monad.Trans             ( liftIO )
+import Control.Monad.Trans ( liftIO )
 
 -- from MonadCatchIO-transformers:
-import Control.Monad.CatchIO           ( MonadCatchIO, bracket )
+import Control.Monad.CatchIO ( MonadCatchIO, bracket )
 
 -- from iteratee:
-import Data.Iteratee.Base              ( EnumeratorGM
-                                       , StreamG( Chunk )
-                                       , IterGV( Done, Cont )
-                                       , runIter
-                                       , enumErr
-                                       , throwErr
-                                       )
-import Data.Iteratee.Base.StreamChunk  ( ReadableChunk (readFromPtr) )
+import Data.Iteratee.Base ( EnumeratorGM
+                          , StreamG( Chunk )
+                          , IterGV( Done, Cont )
+                          , runIter
+                          , enumErr
+                          , throwErr
+                          )
+import Data.Iteratee.Base.StreamChunk ( ReadableChunk (readFromPtr) )
+
+-- from myself:
+import System.USB.DeviceHandling ( DeviceHandle )
+import System.USB.Descriptors    ( EndpointAddress )
+import System.USB.IO.Synchronous ( Timeout, Size )
+import System.USB.Internal       ( C'TransferFunc
+                                 , getDevHndlPtr
+                                 , marshalEndpointAddress
+                                 , convertUSBException
+                                 )
 
 
 --------------------------------------------------------------------------------
+-- Enumerators
+--------------------------------------------------------------------------------
 
-enumReadBulk :: (ReadableChunk s el, MonadCatchIO m)
-             => DeviceHandle       -- ^ A handle for the device to communicate
-                                   --   with.
-             -> EndpointAddress    -- ^ The address of a valid 'In' and 'Bulk'
-                                   --   endpoint to communicate with. Make sure
-                                   --   the endpoint belongs to the current
-                                   --   alternate setting of a claimed interface
-                                   --   which belongs to the device.
-             -> Timeout            -- ^ Timeout (in milliseconds) that this
-                                   --   function should wait for each chunk
-                                   --   before giving up due to no response
-                                   --   being received.  For no timeout, use
-                                   --   value 0.
-             -> Size               -- ^ Chunk size. A good value for this would
-                                   --   be the 'endpointMaxPacketSize'.
-             -> EnumeratorGM s el m a
+enumReadBulk ∷ (ReadableChunk s el, MonadCatchIO m)
+             ⇒ DeviceHandle    -- ^ A handle for the device to communicate with.
+             → EndpointAddress -- ^ The address of a valid 'In' and 'Bulk'
+                               --   endpoint to communicate with. Make sure the
+                               --   endpoint belongs to the current alternate
+                               --   setting of a claimed interface which belongs
+                               --   to the device.
+             → Timeout         -- ^ Timeout (in milliseconds) that this function
+                               --   should wait for each chunk before giving up
+                               --   due to no response being received.  For no
+                               --   timeout, use value 0.
+             → Size            -- ^ Chunk size. A good value for this would be
+                               --   the 'endpointMaxPacketSize'.
+             → EnumeratorGM s el m a
 enumReadBulk = enumRead c'libusb_bulk_transfer
 
-enumReadInterrupt :: (ReadableChunk s el, MonadCatchIO m)
-                  => DeviceHandle       -- ^ A handle for the device to
-                                        --   communicate with.
-                  -> EndpointAddress    -- ^ The address of a valid 'In' and
-                                        --   'Interrupt' endpoint to communicate
-                                        --   with. Make sure the endpoint
-                                        --   belongs to the current alternate
-                                        --   setting of a claimed interface
-                                        --   which belongs to the device.
-                  -> Timeout            -- ^ Timeout (in milliseconds) that this
-                                        --   function should wait for each chunk
-                                        --   before giving up due to no response
-                                        --   being received.  For no timeout,
-                                        --   use value 0.
-                  -> Size               -- ^ Chunk size. A good value for this
-                                        --   would be the 'endpointMaxPacketSize'.
-                  -> EnumeratorGM s el m a
+enumReadInterrupt ∷ (ReadableChunk s el, MonadCatchIO m)
+                  ⇒ DeviceHandle    -- ^ A handle for the device to communicate
+                                    --   with.
+                  → EndpointAddress -- ^ The address of a valid 'In' and
+                                    --   'Interrupt' endpoint to communicate
+                                    --   with. Make sure the endpoint belongs to
+                                    --   the current alternate setting of a
+                                    --   claimed interface which belongs to the
+                                    --   device.
+                  → Timeout         -- ^ Timeout (in milliseconds) that this
+                                    --   function should wait for each chunk
+                                    --   before giving up due to no response
+                                    --   being received.  For no timeout, use
+                                    --   value 0.
+                  → Size            -- ^ Chunk size. A good value for this would
+                                    --   be the 'endpointMaxPacketSize'.
+                  → EnumeratorGM s el m a
 enumReadInterrupt = enumRead c'libusb_interrupt_transfer
 
 
 --------------------------------------------------------------------------------
 
-enumRead :: forall s el m a. (ReadableChunk s el, MonadCatchIO m)
-         => C'TransferFunc -> DeviceHandle
-                           -> EndpointAddress
-                           -> Timeout
-                           -> Size
-                           -> EnumeratorGM s el m a
+enumRead ∷ forall s el m a. (ReadableChunk s el, MonadCatchIO m)
+         ⇒ C'TransferFunc → DeviceHandle
+                          → EndpointAddress
+                          → Timeout
+                          → Size
+                          → EnumeratorGM s el m a
 enumRead c'transfer devHndl
                     endpoint
                     timeout
                     chunkSize
                     iter =
-    genAlloca $ \transferredPtr ->
-        let bufferSize = chunkSize * sizeOf (undefined :: el)
-        in genAllocaBytes bufferSize $ \dataPtr ->
+    genAlloca $ \transferredPtr →
+        let bufferSize = chunkSize ⋅ sizeOf (undefined ∷ el)
+        in genAllocaBytes bufferSize $ \dataPtr →
             let loop i1 = do
-                  err <- liftIO $ c'transfer (getDevHndlPtr devHndl)
-                                             (marshalEndpointAddress endpoint)
-                                             (castPtr dataPtr)
-                                             (fromIntegral bufferSize)
-                                             transferredPtr
-                                             (fromIntegral timeout)
-                  if err /= c'LIBUSB_SUCCESS &&
-                     err /= c'LIBUSB_ERROR_TIMEOUT
+                  err ← liftIO $ c'transfer (getDevHndlPtr devHndl)
+                                            (marshalEndpointAddress endpoint)
+                                            (castPtr dataPtr)
+                                            (fromIntegral bufferSize)
+                                            transferredPtr
+                                            (fromIntegral timeout)
+                  if err ≢ c'LIBUSB_SUCCESS ∧
+                     err ≢ c'LIBUSB_ERROR_TIMEOUT
                     then enumErr (show $ convertUSBException err) i1
                     else do
-                      t <- liftIO $ peek transferredPtr
-                      s <- liftIO $ readFromPtr dataPtr $ fromIntegral t
-                      r <- runIter i1 $ Chunk s
+                      t ← liftIO $ peek transferredPtr
+                      s ← liftIO $ readFromPtr dataPtr $ fromIntegral t
+                      r ← runIter i1 $ Chunk s
                       case r of
-                        Done x _        -> return $ return x
-                        Cont i2 Nothing -> loop i2
-                        Cont _ (Just e) -> return $ throwErr e
+                        Done x _        → return $ return x
+                        Cont i2 Nothing → loop i2
+                        Cont _ (Just e) → return $ throwErr e
             in loop iter
 
 
 --------------------------------------------------------------------------------
+-- Utilities
+--------------------------------------------------------------------------------
 
-genAlloca :: (Storable a, MonadCatchIO m) => (Ptr a -> m b) -> m b
+genAlloca ∷ (Storable a, MonadCatchIO m) ⇒ (Ptr a → m b) → m b
 genAlloca = bracketIO malloc free
 
-genAllocaBytes :: (Storable a, MonadCatchIO m) => Int -> (Ptr a -> m b) -> m b
+genAllocaBytes ∷ (Storable a, MonadCatchIO m) ⇒ Int → (Ptr a → m b) → m b
 genAllocaBytes n = bracketIO (mallocBytes n) free
 
-bracketIO :: MonadCatchIO m => IO a -> (a -> IO c) -> (a -> m b) -> m b
-bracketIO before after = bracket (liftIO before) (liftIO . after)
+bracketIO ∷ MonadCatchIO m ⇒ IO a → (a → IO c) → (a → m b) → m b
+bracketIO before after = bracket (liftIO before) (liftIO ∘ after)
 
 
 -- The End ---------------------------------------------------------------------
