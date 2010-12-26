899abae814e132bdec19f70ed20b22598a8bd4f7
Bas van Dijk <v.dijk.bas@gmail.com>
2010-12-26 12:57:57 +0100

    Import missing (>>) in Utils (only when ghc < 7)
    Ignore-this: f7608c3799ef1c695c3f3bb0997ffcd3
    Reported by Joris Putcuyps.
    Bumped version from 0.6.0.5 to 0.6.0.6 because of this internal change.
    
    darcs-hash:20101226115757-ae560-742216b997491392b873a1657fe36d620031ba61.gz

--- Utils.hs
+++ Utils.hs
@@ -14,6 +14,7 @@ import Prelude               ( (+), (-), (^)
 
 #if __GLASGOW_HASKELL__ < 700
 import Prelude               ( fromInteger )
+import Control.Monad         ( (>>) )
 #endif
 
 import Control.Monad         ( Monad, (>>=), mapM )
