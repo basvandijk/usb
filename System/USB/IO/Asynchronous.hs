f75b41ddaf85c4863bb41306473d443bd6c7b7ce
Bas van Dijk <v.dijk.bas@gmail.com>
2011-04-20 18:03:08 +0200

    Documentation for System.USB.IO.Asynchronous

--- System/USB/IO/Asynchronous.hs
+++ System/USB/IO/Asynchronous.hs
@@ -10,6 +10,11 @@
 -- This module provides functionality for performing control, bulk and interrupt
 -- transfers.
 --
+-- This module provides the exact same API as "System.USB.IO.Synchronous".
+-- However the functions from this module have asynchronous implementations that
+-- integrate with the GHC event manager. This should be more efficient because
+-- it doesn't require busy-loops.
+--
 --------------------------------------------------------------------------------
 
 module System.USB.IO.Asynchronous
@@ -73,28 +78,121 @@ import System.USB.Internal ( DeviceHandle
                            , readBulkAsync, writeBulkAsync
 
                            , readInterruptAsync, writeInterruptAsync
+
+                           , USBException(..)
                            )
 
+{-| Perform a USB /control/ request that does not transfer data.
+
+Exceptions:
+
+ * 'TimeoutException' if the transfer timed out.
+
+ * 'PipeException' if the control request was not supported by the device
+
+ * 'NoDeviceException' if the device has been disconnected.
+
+ *  Another 'USBException'.
+-}
 control ∷ DeviceHandle → ControlAction (Timeout → IO ())
 control = controlAsync
 
+{-| Perform a USB /control/ read.
+
+Exceptions:
+
+ * 'PipeException' if the control request was not supported by the device
+
+ * 'NoDeviceException' if the device has been disconnected.
+
+ *  Another 'USBException'.
+-}
 readControl ∷ DeviceHandle → ControlAction ReadAction
 readControl = readControlAsync
 
+-- | A convenience function similar to 'readControl' which checks if the
+-- specified number of bytes to read were actually read. Throws an 'IOException'
+-- if this is not the case.
 readControlExact ∷ DeviceHandle → ControlAction (Size → Timeout → IO B.ByteString)
 readControlExact = readControlExactAsync
 
+{-| Perform a USB /control/ write.
+
+Exceptions:
+
+ * 'PipeException' if the control request was not supported by the device
+
+ * 'NoDeviceException' if the device has been disconnected.
+
+ *  Another 'USBException'.
+-}
 writeControl ∷ DeviceHandle → ControlAction WriteAction
 writeControl = writeControlAsync
 
+{-| Perform a USB /bulk/ read.
+
+Exceptions:
+
+ * 'PipeException' if the endpoint halted.
+
+ * 'OverflowException' if the device offered more data,
+   see /Packets and overflows/ in the @libusb@ documentation:
+   <http://libusb.sourceforge.net/api-1.0/packetoverflow.html>.
+
+ * 'NoDeviceException' if the device has been disconnected.
+
+ * Another 'USBException'.
+-}
 readBulk ∷ DeviceHandle → EndpointAddress → ReadAction
 readBulk = readBulkAsync
 
+{-| Perform a USB /bulk/ write.
+
+Exceptions:
+
+ * 'PipeException' if the endpoint halted.
+
+ * 'OverflowException' if the device offered more data,
+   see /Packets and overflows/ in the @libusb@ documentation:
+   <http://libusb.sourceforge.net/api-1.0/packetoverflow.html>.
+
+ * 'NoDeviceException' if the device has been disconnected.
+
+ * Another 'USBException'.
+-}
 writeBulk ∷ DeviceHandle → EndpointAddress → WriteAction
 writeBulk = writeBulkAsync
 
+{-| Perform a USB /interrupt/ read.
+
+Exceptions:
+
+ * 'PipeException' if the endpoint halted.
+
+ * 'OverflowException' if the device offered more data,
+   see /Packets and overflows/ in the libusb documentation:
+   <http://libusb.sourceforge.net/api-1.0/packetoverflow.html>.
+
+ * 'NoDeviceException' if the device has been disconnected.
+
+ * Another 'USBException'.
+-}
 readInterrupt ∷ DeviceHandle → EndpointAddress → ReadAction
 readInterrupt = readInterruptAsync
 
+{-| Perform a USB /interrupt/ write.
+
+Exceptions:
+
+ * 'PipeException' if the endpoint halted.
+
+ * 'OverflowException' if the device offered more data,
+   see /Packets and overflows/ in the @libusb@ documentation:
+   <http://libusb.sourceforge.net/api-1.0/packetoverflow.html>.
+
+ * 'NoDeviceException' if the device has been disconnected.
+
+ * Another 'USBException'.
+-}
 writeInterrupt ∷ DeviceHandle → EndpointAddress → WriteAction
 writeInterrupt = writeInterruptAsync
