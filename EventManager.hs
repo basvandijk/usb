57d693580ad40cb53c0f02776ea2f8fe40ac4f2d
Bas van Dijk <v.dijk.bas@gmail.com>
2011-04-27 22:53:06 +0200

    Removed eventManagerIsAvailable from EventManager because it's not used anymore

--- EventManager.hs
+++ EventManager.hs
@@ -1,21 +1,16 @@
 {-# LANGUAGE NoImplicitPrelude, UnicodeSyntax, ForeignFunctionInterface #-}
 
-module EventManager ( eventManagerIsAvailable, getSystemEventManager ) where
+module EventManager ( getSystemEventManager ) where
 
 -- from base:
 import Data.Function    ( ($) )
 import Data.IORef       ( IORef, newIORef, readIORef )
-import Data.Bool        ( Bool )
-import Data.Functor     ( (<$>) )
-import Data.Maybe       ( Maybe(Nothing), isJust )
+import Data.Maybe       ( Maybe(Nothing) )
 import Foreign.Ptr      ( Ptr )
 import GHC.Conc.Sync    ( sharedCAF )
 import System.Event     ( EventManager )
 import System.IO        ( IO )
 import System.IO.Unsafe ( unsafePerformIO )
-
-eventManagerIsAvailable ∷ IO Bool
-eventManagerIsAvailable = isJust <$> getSystemEventManager
 
 getSystemEventManager ∷ IO (Maybe EventManager)
 getSystemEventManager = readIORef eventManager
